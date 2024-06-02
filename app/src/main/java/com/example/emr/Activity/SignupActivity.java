efbd3990c16124c42a80004fba213a43e6299b20
emrecankarli <emrecankarli88@gmail.com>
2024-06-02 17:11:12 +0300

    UPDATE
    Added registration and login screen using Firebase (Homepage under construction)
    
    GÜNCELLEME
    
    Firebase kullanılarak kayıt ve giriş ekranı eklendi. (Anasayfa yapım aşamasında)

--- app/src/main/java/com/example/emr/Activity/SignupActivity.java
+++ app/src/main/java/com/example/emr/Activity/SignupActivity.java
@@ -2,29 +2,34 @@ package com.example.emr.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.emr.R;
-import com.example.emr.databinding.ActivityLoginBinding;
+import com.example.emr.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends BaseActivity {
-    ActivityLoginBinding binding;
+public class SignupActivity extends BaseActivity {
+    ActivitySignupBinding binding;
     private FirebaseAuth mAuth;
-    private static final String TAG = "LoginActivity";
+    private static final String TAG = "SignupActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // FirebaseAuth örneğini al
@@ -42,20 +47,24 @@ public class LoginActivity extends BaseActivity {
     }
 
     private void setVariable() {
-        binding.loginBtn.setOnClickListener(v -> {
+        binding.signupBtn.setOnClickListener(v -> {
             String email = binding.userEdit.getText().toString();
             String password = binding.passwordEdit.getText().toString();
-            if (!email.isEmpty() && !password.isEmpty()){
-                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, task -> {
-                    if (task.isSuccessful()){
-                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                    } else {
-                        Toast.makeText(LoginActivity.this, "Doğrulama Başarısız", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            } else {
-                Toast.makeText(LoginActivity.this, "Lütfen email ve şifre giriniz", Toast.LENGTH_SHORT).show();
+
+            if (password.length() < 6) {
+                Toast.makeText(SignupActivity.this, "Şifre en az 6 karakter olmalı", Toast.LENGTH_SHORT).show();
+                return;
             }
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignupActivity.this, task -> {
+                if (task.isSuccessful()) {
+                    Log.i(TAG, "onComplete: ");
+                    startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                } else {
+                    Log.i(TAG, "failure: " + task.getException());
+                    Toast.makeText(SignupActivity.this, "Doğrulama Başarısız", Toast.LENGTH_SHORT).show();
+                }
+            });
         });
     }
 }
