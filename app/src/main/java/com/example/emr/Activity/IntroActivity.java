99bdb4ccfbe979bc20902fab5109350ee60ac128
emrecankarli <emrecankarli88@gmail.com>
2024-06-02 14:33:08 +0300

    Update - Intro page added and logo added (Login and registration page under construction)
    Güncelleme - İntro sayfası eklendi ve logo eklendi (Giriş ve kayıt sayfası yapım aşamasında)

--- app/src/main/java/com/example/emr/Activity/IntroActivity.java
+++ app/src/main/java/com/example/emr/Activity/IntroActivity.java
@@ -1,6 +1,8 @@
 package com.example.emr.Activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +11,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.emr.R;
+import com.example.emr.databinding.ActivityIntroBinding;
 
-public class IntroActivity extends AppCompatActivity {
-
+public class IntroActivity extends BaseActivity {
+    ActivityIntroBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_intro);
+        setContentView(binding.getRoot());
+
+        setVariable();
+        getWindow().setStatusBarColor(Color.parseColor("#FFE485"));
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void setVariable() {
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
 }
