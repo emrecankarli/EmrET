33bc751d9ac2f5315e780faa5e073903a0087036
emrecankarli <emrecankarli88@gmail.com>
2024-06-03 18:21:00 +0300

    UPDATE
    Homepage updated.
    
    GÜNCELLEME
    
    Ana Sayfa Güncellendi.

---
+++ app/src/main/java/com/example/emr/Domain/Category.java
@@ -0,0 +1,35 @@
+package com.example.emr.Domain;
+
+public class Category {
+    private int id;
+
+    public Category() {
+    }
+
+    private String ImagePath;
+    private String Name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImagePath() {
+        return ImagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        ImagePath = imagePath;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
